3698fa870e00559d76b044fab82571248631352e
Egor <72040294+iGooseRU@users.noreply.github.com>
2022-02-14 12:57:49 +0300

    Delete SQLQuery3.sql

--- SQLQuery3.sql
+++
@@ -1,8 +0,0 @@
--- Найти и вывести на экран название, цвет и размер таких продуктов, у которых цена менее 100 и цвет Black
-select
-	p.Name,
-	p.Color,
-	P.Size
-
-from Production.Product as p
-where p.ListPrice < 100 and p.Color in ('Black')
