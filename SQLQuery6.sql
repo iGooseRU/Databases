61793afc81e4435cc30a0686a9a56d9d8a02b943
Egor <72040294+iGooseRU@users.noreply.github.com>
2022-02-14 12:58:13 +0300

    Delete SQLQuery6.sql

--- SQLQuery6.sql
+++
@@ -1,9 +0,0 @@
--- Найти и вывести на экран название и цвет таких продуктов, для которых определен и цвет, и размер
-select
-	p.Name,
-	p.Color
-
-from Production.Product as p
-
-where p.Color is not null
-		and p.Size is not null
